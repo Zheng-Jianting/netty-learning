cc6e12b0475ac37035420be51c09c625f7ec44c6
郑健霆 <374537202@qq.com>
2022-06-04 15:33:17 +0800

    第4章 传输

---
+++ netty/src/main/java/com/zhengjianting/nia/chapter4/PlainOioServer.java
@@ -0,0 +1,45 @@
+package com.zhengjianting.nia.chapter4;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class PlainOioServer {
+    public void server(int port) throws IOException {
+        final ServerSocket socket = new ServerSocket(port);
+        try {
+            for (;;) {
+                final Socket clientSocket = socket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream out;
+                        try {
+                            out = clientSocket.getOutputStream();
+                            out.write("Hi!\r\n".getBytes(StandardCharsets.UTF_8));
+                            out.flush();
+                            clientSocket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new PlainOioServer().server(10526);
+    }
+}
